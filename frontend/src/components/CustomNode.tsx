c8400ac9b9161327d4f7a9ba5bf113e7c833a015
Antonello Finucci <30832019+Afinucci@users.noreply.github.com>
2025-07-17 16:04:23 +0200

    Fix duplicate handle IDs in CustomNode - each handle now has unique ID for proper ReactFlow connection handling

--- frontend/src/components/CustomNode.tsx
+++ frontend/src/components/CustomNode.tsx
@@ -84,10 +84,109 @@ const CustomNode: React.FC<NodeProps<CustomNodeData>> = ({ data, selected }) => 
         },
       }}
     >
-      <Handle type="target" position={Position.Left} />
-      <Handle type="source" position={Position.Right} />
-      <Handle type="target" position={Position.Top} />
-      <Handle type="source" position={Position.Bottom} />
+      {/* Left handle - can connect both ways */}
+      <Handle 
+        type="source" 
+        position={Position.Left} 
+        id="left-source"
+        style={{ 
+          background: '#555',
+          width: 10,
+          height: 10,
+          border: '2px solid #fff',
+          borderRadius: '50%'
+        }}
+      />
+      <Handle 
+        type="target" 
+        position={Position.Left} 
+        id="left-target"
+        style={{ 
+          background: '#555',
+          width: 10,
+          height: 10,
+          border: '2px solid #fff',
+          borderRadius: '50%'
+        }}
+      />
+      
+      {/* Right handle - can connect both ways */}
+      <Handle 
+        type="source" 
+        position={Position.Right} 
+        id="right-source"
+        style={{ 
+          background: '#555',
+          width: 10,
+          height: 10,
+          border: '2px solid #fff',
+          borderRadius: '50%'
+        }}
+      />
+      <Handle 
+        type="target" 
+        position={Position.Right} 
+        id="right-target"
+        style={{ 
+          background: '#555',
+          width: 10,
+          height: 10,
+          border: '2px solid #fff',
+          borderRadius: '50%'
+        }}
+      />
+      
+      {/* Top handle - can connect both ways */}
+      <Handle 
+        type="source" 
+        position={Position.Top} 
+        id="top-source"
+        style={{ 
+          background: '#555',
+          width: 10,
+          height: 10,
+          border: '2px solid #fff',
+          borderRadius: '50%'
+        }}
+      />
+      <Handle 
+        type="target" 
+        position={Position.Top} 
+        id="top-target"
+        style={{ 
+          background: '#555',
+          width: 10,
+          height: 10,
+          border: '2px solid #fff',
+          borderRadius: '50%'
+        }}
+      />
+      
+      {/* Bottom handle - can connect both ways */}
+      <Handle 
+        type="source" 
+        position={Position.Bottom} 
+        id="bottom-source"
+        style={{ 
+          background: '#555',
+          width: 10,
+          height: 10,
+          border: '2px solid #fff',
+          borderRadius: '50%'
+        }}
+      />
+      <Handle 
+        type="target" 
+        position={Position.Bottom} 
+        id="bottom-target"
+        style={{ 
+          background: '#555',
+          width: 10,
+          height: 10,
+          border: '2px solid #fff',
+          borderRadius: '50%'
+        }}
+      />
       
       {/* Group indicator */}
       {groupId && (
